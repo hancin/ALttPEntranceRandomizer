d9bf1fd74a53db638280224d396180b12697f617
Hancin <hancin@hancinworld.com>
2019-07-18 15:48:59 -0400

    Revert "Remove the "an item cannot be locked someone else's same item" rule"
    This reverts commit 913d3baac9ac170e59512899afd1d39d865b49e6.

--- Fill.py
+++ Fill.py
@@ -161,7 +161,7 @@ def distribute_items_staleness(world):
     logging.getLogger('').debug('Unplaced items: %s - Unfilled Locations: %s', [item.name for item in itempool], [location.name for location in fill_locations])
 
 
-def fill_restrictive(world, base_state, locations, itempool):
+def fill_restrictive(world, base_state, locations, itempool, multiworld_restriction=False):
     def sweep_from_pool():
         new_state = base_state.copy()
         for item in itempool:
@@ -170,15 +170,15 @@ def fill_restrictive(world, base_state, locations, itempool):
         return new_state
 
     while itempool and locations:
+        item_name = itempool[-1].name
         items_to_place = []
-        nextpool = []
         placing_players = set()
-        for item in reversed(itempool):
-            if item.player not in placing_players:
+        nextpool = itempool.copy()
+        for index, item in enumerate(itempool):
+            if item.name == item_name and item.player not in placing_players:
                 placing_players.add(item.player)
+                nextpool.remove(item)
                 items_to_place.append(item)
-            else:
-                nextpool.insert(0, item)
         itempool = nextpool
 
         maximum_exploration_state = sweep_from_pool()
@@ -187,6 +187,7 @@ def fill_restrictive(world, base_state, locations, itempool):
         if world.check_beatable_only:
             perform_access_check = not world.has_beaten_game(maximum_exploration_state)
 
+        filled_locations = []
         for item_to_place in items_to_place:
             spot_to_fill = None
             for location in locations:
@@ -205,6 +206,12 @@ def fill_restrictive(world, base_state, locations, itempool):
             world.push_item(spot_to_fill, item_to_place, False)
             locations.remove(spot_to_fill)
             spot_to_fill.event = True
+
+            if multiworld_restriction:
+                spot_to_fill.location_dependencies = filled_locations.copy()
+                for location in filled_locations:
+                    location.location_dependencies.append(spot_to_fill)
+                filled_locations.append(spot_to_fill)
 
 
 def distribute_items_restrictive(world, gftower_trash_count=0, fill_locations=None):
@@ -239,7 +246,7 @@ def distribute_items_restrictive(world, gftower_trash_count=0, fill_locations=No
     if world.keysanity and world.mode == 'standard':
         progitempool.sort(key=lambda item: 1 if item.name == 'Small Key (Escape)' else 0)
 
-    fill_restrictive(world, world.state, fill_locations, progitempool)
+    fill_restrictive(world, world.state, fill_locations, progitempool, True)
 
     random.shuffle(fill_locations)
 
